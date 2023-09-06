4e273afae3e7345644d6277f88281d5a120bfa8d
Charles Moore <122481442+moorec-aws@users.noreply.github.com>
2023-09-06 15:44:35 -0500

    Fix depsBundle.sh zip name (#12)
    Signed-off-by: Charles Moore <moorec@amazon.com>

--- depsBundle.sh
+++ depsBundle.sh
@@ -3,10 +3,10 @@ set -xeuo pipefail
 
 python depsBundle.py
 
-rm -f dependency_bundle/deadline_submitter_for_nuke-deps-windows.zip
-rm -f dependency_bundle/deadline_submitter_for_nuke-deps-linux.zip
-rm -f dependency_bundle/deadline_submitter_for_nuke-deps-macos.zip
+rm -f dependency_bundle/deadline_cloud_for_nuke_submitter-deps-windows.zip
+rm -f dependency_bundle/deadline_cloud_for_nuke_submitter-deps-linux.zip
+rm -f dependency_bundle/deadline_cloud_for_nuke_submitter-deps-macos.zip
 
-cp dependency_bundle/deadline_submitter_for_nuke-deps.zip dependency_bundle/deadline_submitter_for_nuke-deps-windows.zip
-cp dependency_bundle/deadline_submitter_for_nuke-deps.zip dependency_bundle/deadline_submitter_for_nuke-deps-linux.zip
-cp dependency_bundle/deadline_submitter_for_nuke-deps.zip dependency_bundle/deadline_submitter_for_nuke-deps-macos.zip
+cp dependency_bundle/deadline_cloud_for_nuke_submitter-deps.zip dependency_bundle/deadline_cloud_for_nuke_submitter-deps-windows.zip
+cp dependency_bundle/deadline_cloud_for_nuke_submitter-deps.zip dependency_bundle/deadline_cloud_for_nuke_submitter-deps-linux.zip
+cp dependency_bundle/deadline_cloud_for_nuke_submitter-deps.zip dependency_bundle/deadline_cloud_for_nuke_submitter-deps-macos.zip
