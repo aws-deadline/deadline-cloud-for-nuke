0bcec015784c087b5928be0b9c0df21e95ec286f
Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>
2023-10-11 15:39:40 -0700

    testing an easier fix
    Signed-off-by: Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>

--- test/unit/__init__.py
+++ test/unit/__init__.py
@@ -1,10 +1,11 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 import sys
-from unittest.mock import MagicMock, Mock
+from unittest.mock import MagicMock  # , Mock
 
 # we must mock nuke and UI code
 mock_modules = [
+    "deadline.client.ui.deadline_credentials_status",
     "nuke",
     "nuke.Node",
     "ui.deadline_submitter",
@@ -17,10 +18,3 @@ mock_modules = [
 
 for module in mock_modules:
     sys.modules[module] = MagicMock()
-
-# Mock the call to DeadlineCredentialsStatus.getInstance that happens at the module level
-# in submit_job_to_deadline_dialog.py. That call is done at the module level to gather the
-# status before the dialog is opened.
-from deadline.client.ui.deadline_credentials_status import DeadlineCredentialsStatus  # noqa: E402
-
-DeadlineCredentialsStatus.getInstance = Mock(return_value=None)
