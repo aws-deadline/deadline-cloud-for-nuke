215ff64c5a225f45ca323d55f34fee37ee596197
Nathan MacLeod <142927985+npmacl@users.noreply.github.com>
2025-08-21 12:43:40 -0700

    fix: ran format
    Signed-off-by: Nathan MacLeod <142927985+npmacl@users.noreply.github.com>

--- src/deadline/nuke_submitter/assets.py
+++ src/deadline/nuke_submitter/assets.py
@@ -15,8 +15,8 @@ from deadline.nuke_util import ocio as nuke_ocio
 
 # Nuke allows the use of printf style expressions in path names to be evaluated to the current frame number
 # and view being rendered. For example %04d would be the current frame number zero padded to be at least 4 digits.
-# %v or %V will be evaluated to the first character, or full name of the current view. 
-# 
+# %v or %V will be evaluated to the first character, or full name of the current view.
+#
 # Note Nuke also allows the use of '####' to describe a frame number as well, but we do not need to match
 # this since the knob.value() translates it to an equivalent printf style expression.
 # E.g. frame_####.exr becomes frame_%04d.exr.
@@ -187,7 +187,9 @@ def get_output_paths_for_filenode(node) -> set[IOPath]:
         if expression_match:
             # in the case of an expression for frames / views, we will used the parent directory
             # of the filenode containing the first expression
-            nuke.tprint(f"found printf style expression {expression_match.group(1)} starting at index {expression_match.start()} in path {filepath}")
+            nuke.tprint(
+                f"found printf style expression {expression_match.group(1)} starting at index {expression_match.start()} in path {filepath}"
+            )
 
             pos = expression_match.start()
             # walk back to the nearest /
@@ -195,7 +197,9 @@ def get_output_paths_for_filenode(node) -> set[IOPath]:
                 pos -= 1
 
             if pos == -1:
-                nuke.tprint("Did not find a written directory above the filenode where the expression appears. Using ./ as the output path.")
+                nuke.tprint(
+                    "Did not find a written directory above the filenode where the expression appears. Using ./ as the output path."
+                )
                 filepath = "./"
             else:
                 filepath = filepath[: pos + 1]
@@ -232,8 +236,10 @@ def evaluate_tcl_subexpressions(string: str) -> str:
                 # end tcl expression
                 expression = "".join(current_tcl_expression)
                 expression_evaluation = nuke.tcl(expression)
-                
-                nuke.tprint(f"Parsed expression [{expression}] in string {string}, which evaluated to {expression_evaluation}")
+
+                nuke.tprint(
+                    f"Parsed expression [{expression}] in string {string}, which evaluated to {expression_evaluation}"
+                )
 
                 evaluated_string.append(expression_evaluation)
                 current_tcl_expression = []  # reset for any subsequent expressions
