8c9e195d6827b12150b0876ec5954cdc5a8a5959
MacLeod <142927985+npmacl@users.noreply.github.com>
2025-12-29 18:26:49 +0000

    fix formatting error
    Signed-off-by: MacLeod <142927985+npmacl@users.noreply.github.com>

--- src/deadline/nuke_adaptor/copycat_adaptor.py
+++ src/deadline/nuke_adaptor/copycat_adaptor.py
@@ -13,23 +13,27 @@ error_regex = re.compile(r".*ERROR: (.+)")
 step_complete_regex = re.compile(r"\[Step:(\d+)/(\d+)\].*")
 
 _current_percent_done = 0
+
+
 def _report_progress(current_step, total_steps):
     global _current_percent_done
     # round progress to a single decimal point
     progress = round(current_step * 100.0 / total_steps, 1)
     if progress != _current_percent_done:
         _current_percent_done = progress
-        return f'openjd_progress: {progress}'
+        return f"openjd_progress: {progress}"
 
 
 _error_encountered = False
+
+
 # returning message rather than printing to make this unit testable
 def report_openjd_messages(line) -> Optional[str]:
     global _error_encountered
     if (match := error_regex.match(line)) is not None:
         # error message is captured in group(1)
         _error_encountered = True
-        return f'openjd_fail: {match.group(1)}'
+        return f"openjd_fail: {match.group(1)}"
     if (match := step_complete_regex.match(line)) is not None:
         return _report_progress(current_step=int(match.group(1)), total_steps=int(match.group(2)))
 
@@ -41,46 +45,48 @@ def _stream_reader(stream_name, stream, logger):
             # msg will be the str of the openjd message if there is something to print,
             # None otherwise
             logger.info(msg)
-        logger.info(f'{stream_name}: {line_stripped}')
+        logger.info(f"{stream_name}: {line_stripped}")
 
 
 def get_nuke_remap_string(path_mapping_rules: List[Dict[str, str]]) -> str:
-    return ','.join(
+    return ",".join(
         pathlib.Path(path).as_posix()
         for rule in path_mapping_rules
-        for path in (rule['source_path'], rule['destination_path'])
+        for path in (rule["source_path"], rule["destination_path"])
     )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        prog = 'NukeCopyCatAdaptor',
-        description = (
-            'Wrapper around executing CopyCat nodes in Nuke. Handle path mapping from job attachments'
-            ' and emmision of OpenJD status messages to stdout'
+        prog="NukeCopyCatAdaptor",
+        description=(
+            "Wrapper around executing CopyCat nodes in Nuke. Handle path mapping from job attachments"
+            " and emmision of OpenJD status messages to stdout"
         ),
     )
 
-    parser.add_argument('--nuke', type=pathlib.Path, help='Path to the Nuke exe.')
-    parser.add_argument('--path-mapping-rules', type=pathlib.Path, help='Path to path-mapping rules file.')
-    parser.add_argument('--nuke-script', type=pathlib.Path, help='Path to the nuke script file.')
-    parser.add_argument('--copycat-node', type=str, help='Name of the copycat node to train.')
+    parser.add_argument("--nuke", type=pathlib.Path, help="Path to the Nuke exe.")
     parser.add_argument(
-        '--run-as-shell',
-        action='store_true',
+        "--path-mapping-rules", type=pathlib.Path, help="Path to path-mapping rules file."
+    )
+    parser.add_argument("--nuke-script", type=pathlib.Path, help="Path to the nuke script file.")
+    parser.add_argument("--copycat-node", type=str, help="Name of the copycat node to train.")
+    parser.add_argument(
+        "--run-as-shell",
+        action="store_true",
         default=False,
-        help='Uses shell=true when launching the passed in executable. This just exists for unit testing'
+        help="Uses shell=true when launching the passed in executable. This just exists for unit testing",
     )
 
     args = parser.parse_args()
 
     nuke_run_copycat_args = [
         str(args.nuke),
-        '-X',
+        "-X",
         args.copycat_node,
-        '-F', # when running copycat we specify to execute only a single "frame"
-        '1',
-        '--gpu',
+        "-F",  # when running copycat we specify to execute only a single "frame"
+        "1",
+        "--gpu",
         str(args.nuke_script),
     ]
 
@@ -91,7 +97,7 @@ if __name__ == "__main__":
         nuke_path_mapping_string = get_nuke_remap_string(path_mapping_rules)
 
         nuke_run_copycat_args += [
-            '--remap',
+            "--remap",
             nuke_path_mapping_string,
         ]
 
@@ -100,17 +106,21 @@ if __name__ == "__main__":
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         shell=args.run_as_shell,
-        text=True
+        text=True,
     )
 
-    logging.basicConfig(format='%(message)s', stream=sys.stdout)
+    logging.basicConfig(format="%(message)s", stream=sys.stdout)
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
-    stdout_reader = threading.Thread(target=_stream_reader, args=('STDOUT', nuke_process.stdout, logger))
+    stdout_reader = threading.Thread(
+        target=_stream_reader, args=("STDOUT", nuke_process.stdout, logger)
+    )
     stdout_reader.daemon = False
 
-    stderr_reader = threading.Thread(target=_stream_reader, args=('STDERR', nuke_process.stderr, logger))
+    stderr_reader = threading.Thread(
+        target=_stream_reader, args=("STDERR", nuke_process.stderr, logger)
+    )
     stderr_reader.daemon = False
 
     stdout_reader.start()
--- test/unit/deadline_adaptor_for_nuke/CopyCatAdaptor/stubbed_nuke.py
+++ test/unit/deadline_adaptor_for_nuke/CopyCatAdaptor/stubbed_nuke.py
@@ -10,53 +10,81 @@ from typing import Any
 @dataclasses.dataclass
 class Line:
     text: str
-    dest: Any # _io.TextIOWrapper
+    dest: Any  # _io.TextIOWrapper
+
 
 happy_case_lines = [
-    Line(text='Nuke 16.0v1, 64 bit, built Feb 24 2025.', dest=sys.stdout),
-    Line(text='Copyright (c) 2025 The Foundry Visionmongers Ltd.  All Rights Reserved.', dest=sys.stdout),
-    Line(text='Licence expires on: 2026/3/18', dest=sys.stdout),
-    Line(text='[19:28.55] Warning: /sessions/session-bb94863359344b398d92e0f41af024a5dffrdzmi/assetroot-8ec6f7889c962aeaebe1/Downloads/plushie_colorchange/copycat.nk is for nuke16.0v7; this is nuke16.0v1', dest=sys.stderr),
-    Line(text='', dest=sys.stdout),
-    Line(text='[Step:1/30] Loss:0.022428', dest=sys.stdout),
-    Line(text='[Step:10/30] Loss:0.022428', dest=sys.stdout),
-    Line(text='[Step:20/30] Loss:0.0164353', dest=sys.stdout),
-    Line(text='[Step:30/30] Loss:0.0164353', dest=sys.stdout),
-    Line(text=' [*] Cat file saved at /sessions/session-bb94863359344b398d92e0f41af024a5dffrdzmi/assetroot-8ec6f7889c962aeaebe1/Downloads/plushie_colorchange/data_dir/Training_251223_192904.22500.cat', dest=sys.stdout),
-    Line(text=' [*] Optimiser saved at /sessions/session-bb94863359344b398d92e0f41af024a5dffrdzmi/assetroot-8ec6f7889c962aeaebe1/Downloads/plushie_colorchange/data_dir/Training_251223_192904.22500.ost', dest=sys.stdout),
-    Line(text=' [*] Contactsheet saved at /sessions/session-bb94863359344b398d92e0f41af024a5dffrdzmi/assetroot-8ec6f7889c962aeaebe1/Downloads/plushie_colorchange/data_dir/Training_251223_192904.22500.png', dest=sys.stdout),
-    Line(text='', dest=sys.stdout),
-    Line(text='Frame 1 (1 of 1)', dest=sys.stdout),
-    Line(text='', dest=sys.stdout),
-    Line(text='Total render time: 19 minutes, 42 seconds', dest=sys.stdout),
+    Line(text="Nuke 16.0v1, 64 bit, built Feb 24 2025.", dest=sys.stdout),
+    Line(
+        text="Copyright (c) 2025 The Foundry Visionmongers Ltd.  All Rights Reserved.",
+        dest=sys.stdout,
+    ),
+    Line(text="Licence expires on: 2026/3/18", dest=sys.stdout),
+    Line(
+        text="[19:28.55] Warning: /sessions/session-bb94863359344b398d92e0f41af024a5dffrdzmi/assetroot-8ec6f7889c962aeaebe1/Downloads/plushie_colorchange/copycat.nk is for nuke16.0v7; this is nuke16.0v1",
+        dest=sys.stderr,
+    ),
+    Line(text="", dest=sys.stdout),
+    Line(text="[Step:1/30] Loss:0.022428", dest=sys.stdout),
+    Line(text="[Step:10/30] Loss:0.022428", dest=sys.stdout),
+    Line(text="[Step:20/30] Loss:0.0164353", dest=sys.stdout),
+    Line(text="[Step:30/30] Loss:0.0164353", dest=sys.stdout),
+    Line(
+        text=" [*] Cat file saved at /sessions/session-bb94863359344b398d92e0f41af024a5dffrdzmi/assetroot-8ec6f7889c962aeaebe1/Downloads/plushie_colorchange/data_dir/Training_251223_192904.22500.cat",
+        dest=sys.stdout,
+    ),
+    Line(
+        text=" [*] Optimiser saved at /sessions/session-bb94863359344b398d92e0f41af024a5dffrdzmi/assetroot-8ec6f7889c962aeaebe1/Downloads/plushie_colorchange/data_dir/Training_251223_192904.22500.ost",
+        dest=sys.stdout,
+    ),
+    Line(
+        text=" [*] Contactsheet saved at /sessions/session-bb94863359344b398d92e0f41af024a5dffrdzmi/assetroot-8ec6f7889c962aeaebe1/Downloads/plushie_colorchange/data_dir/Training_251223_192904.22500.png",
+        dest=sys.stdout,
+    ),
+    Line(text="", dest=sys.stdout),
+    Line(text="Frame 1 (1 of 1)", dest=sys.stdout),
+    Line(text="", dest=sys.stdout),
+    Line(text="Total render time: 19 minutes, 42 seconds", dest=sys.stdout),
 ]
 
 fail_case_lines = [
-    Line(text='Nuke 16.0v1, 64 bit, built Feb 24 2025.', dest=sys.stdout),
-    Line(text='Copyright (c) 2025 The Foundry Visionmongers Ltd.  All Rights Reserved.', dest=sys.stdout),
-    Line(text='Licence expires on: 2026/3/18', dest=sys.stdout),
-    Line(text='[19:28.55] Warning: /sessions/session-bb94863359344b398d92e0f41af024a5dffrdzmi/assetroot-8ec6f7889c962aeaebe1/Downloads/plushie_colorchange/copycat.nk is for nuke16.0v7; this is nuke16.0v1', dest=sys.stderr),
-    Line(text='Nuke 16.0v1, 64 bit, built Feb 24 2025.', dest=sys.stdout),
-    Line(text='Copyright (c) 2025 The Foundry Visionmongers Ltd.  All Rights Reserved.', dest=sys.stdout),
-    Line(text='Licence expires on: 2026/3/18', dest=sys.stdout),
-    Line(text='[18:41.54] ERROR: CopyCat1: No valid or writable data directory is selected.', dest=sys.stderr),
-    Line(text='.1.2.3.4.5.6.7.8.9', dest=sys.stdout),
-    Line(text='Total render time: 0.00 seconds', dest=sys.stdout),
-    Line(text='CopyCat1: No valid or writable data directory is selected.', dest=sys.stderr),
+    Line(text="Nuke 16.0v1, 64 bit, built Feb 24 2025.", dest=sys.stdout),
+    Line(
+        text="Copyright (c) 2025 The Foundry Visionmongers Ltd.  All Rights Reserved.",
+        dest=sys.stdout,
+    ),
+    Line(text="Licence expires on: 2026/3/18", dest=sys.stdout),
+    Line(
+        text="[19:28.55] Warning: /sessions/session-bb94863359344b398d92e0f41af024a5dffrdzmi/assetroot-8ec6f7889c962aeaebe1/Downloads/plushie_colorchange/copycat.nk is for nuke16.0v7; this is nuke16.0v1",
+        dest=sys.stderr,
+    ),
+    Line(text="Nuke 16.0v1, 64 bit, built Feb 24 2025.", dest=sys.stdout),
+    Line(
+        text="Copyright (c) 2025 The Foundry Visionmongers Ltd.  All Rights Reserved.",
+        dest=sys.stdout,
+    ),
+    Line(text="Licence expires on: 2026/3/18", dest=sys.stdout),
+    Line(
+        text="[18:41.54] ERROR: CopyCat1: No valid or writable data directory is selected.",
+        dest=sys.stderr,
+    ),
+    Line(text=".1.2.3.4.5.6.7.8.9", dest=sys.stdout),
+    Line(text="Total render time: 0.00 seconds", dest=sys.stdout),
+    Line(text="CopyCat1: No valid or writable data directory is selected.", dest=sys.stderr),
 ]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # use the argument passed into nuke_script to decide whether we should output
     # the happy case, or the failure case
-    parser.add_argument('nuke_script')
-    parser.add_argument('-X')
-    parser.add_argument('-F')
-    parser.add_argument('--gpu', action='store_true')
+    parser.add_argument("nuke_script")
+    parser.add_argument("-X")
+    parser.add_argument("-F")
+    parser.add_argument("--gpu", action="store_true")
 
     args = parser.parse_args()
 
-    lines = happy_case_lines if args.nuke_script == 'happy_case' else fail_case_lines
+    lines = happy_case_lines if args.nuke_script == "happy_case" else fail_case_lines
 
     for line in lines:
         print(line.text, file=line.dest)
