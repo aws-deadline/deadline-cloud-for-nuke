cf0aaecc9318496f2a36b722df4258bf836dfe60
Nathan MacLeod <142927985+npmacl@users.noreply.github.com>
2026-01-15 18:52:22 +0000

    move submit copycat out of developer options, not job bundle tests
    Signed-off-by: Nathan MacLeod <142927985+npmacl@users.noreply.github.com>

--- src/menu.py
+++ src/menu.py
@@ -25,15 +25,15 @@ try:
         "Submit to Deadline Cloud", lambda: show_nuke_render_submitter(JobType.RENDER), ""
     )
     aws_deadline_menu.addCommand(
-        "Run Nuke Submitter Job Bundle Output Tests...",
-        run_render_submitter_job_bundle_output_test,
+        "Submit CopyCat Training to Deadline Cloud",
+        lambda: show_nuke_render_submitter(JobType.COPYCAT_TRAINING),
         "",
     )
     # Set the environment variable DEADLINE_ENABLE_DEVELOPER_OPTIONS to "true" to get this menu.
     if os.environ.get("DEADLINE_ENABLE_DEVELOPER_OPTIONS", "").upper() == "TRUE":
         aws_deadline_menu.addCommand(
-            "Submit CopyCat Training to Deadline Cloud",
-            lambda: show_nuke_render_submitter(JobType.COPYCAT_TRAINING),
+            "Run Nuke Submitter Job Bundle Output Tests...",
+            run_render_submitter_job_bundle_output_test,
             "",
         )
 
