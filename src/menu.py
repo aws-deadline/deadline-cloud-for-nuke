fe9aa63e7b7d701dd3f08ca549e33f01b2682dae
Nathan MacLeod <142927985+npmacl@users.noreply.github.com>
2026-01-15 18:05:36 +0000

    unhide copycat submit from dev flags
    Signed-off-by: Nathan MacLeod <142927985+npmacl@users.noreply.github.com>

--- src/menu.py
+++ src/menu.py
@@ -24,16 +24,16 @@ try:
     aws_deadline_menu.addCommand(
         "Submit to Deadline Cloud", lambda: show_nuke_render_submitter(JobType.RENDER), ""
     )
+    aws_deadline_menu.addCommand(
+        "Run Nuke Submitter Job Bundle Output Tests...",
+        run_render_submitter_job_bundle_output_test,
+        "",
+    )
     # Set the environment variable DEADLINE_ENABLE_DEVELOPER_OPTIONS to "true" to get this menu.
     if os.environ.get("DEADLINE_ENABLE_DEVELOPER_OPTIONS", "").upper() == "TRUE":
         aws_deadline_menu.addCommand(
             "Submit CopyCat Training to Deadline Cloud",
             lambda: show_nuke_render_submitter(JobType.COPYCAT_TRAINING),
-            "",
-        )
-        aws_deadline_menu.addCommand(
-            "Run Nuke Submitter Job Bundle Output Tests...",
-            run_render_submitter_job_bundle_output_test,
             "",
         )
 
